de44b7af73745f75febd21a88430169917149f8f
renato1982 <renatocoy@gmail.com>
2023-11-25 16:42:19 -0300

    add cors

--- src/main/java/br/com/pix/tcc/config/WebConfig.java
+++ src/main/java/br/com/pix/tcc/config/WebConfig.java
@@ -1,15 +1,17 @@
 package br.com.pix.tcc.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping( "/**")
+        registry.addMapping("/**")
+                .allowedHeaders("Access-Control-Allow-Origin","*")
                 .allowedMethods("GET","POST","DELETE","OPTIONS","HEAD","TRACE")
-                .allowedHeaders("*")
                 .allowedOrigins("*");
     }
 
